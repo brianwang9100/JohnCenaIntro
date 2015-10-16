9eaa584f5239a045ab982b2bc969cf801d3a82c5
Brian Wang <brianwang9100@gmail.com>
2015-10-15 20:43:52 -0400

    first john cena

---
+++ JohnCenaIntro.spritebuilder/JohnCena.swift
@@ -0,0 +1,13 @@
+//
+//  JohnCena.swift
+//  JohnCenaIntro
+//
+//  Created by Brian Wang on 10/15/15.
+//  Copyright © 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class JohnCena: CCNode {
+
+}
--- JohnCenaIntro.spritebuilder/Source/MainScene.swift
+++ JohnCenaIntro.spritebuilder/Source/MainScene.swift
@@ -1,5 +1,32 @@
 import Foundation
+import AVFoundation
 
 class MainScene: CCNode {
-
+    
+    var audioPlayer = AVAudioPlayer()
+    var johnCena:JohnCena!
+    
+    func didLoadFromCCB() {
+        playIntro()
+        self.scheduleOnce(Selector("explode"), delay: 0.5)
+    }
+    
+    func playIntro() {
+        var johnCenaURL = NSURL.init(fileURLWithPath: NSBundle.mainBundle().pathForResource("JohnCenaIntro", ofType: "mp3")!)
+        do {
+            try audioPlayer = AVAudioPlayer.init(contentsOfURL: johnCenaURL)
+        } catch {
+            print("error")
+        }
+        
+        if (audioPlayer.prepareToPlay()) {
+            audioPlayer.play()
+        }
+    }
+    
+    func explode() {
+        var explosion = CCBReader.load("ExplosionParticle")
+        explosion.position = johnCena.position
+        johnCena.addChild(explosion)
+    }
 }
